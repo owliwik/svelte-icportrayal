289d55aa1e0d441f3c78c46c3e9a223b74968757
Owliwik Li <michael@owliwik.com>
2024-10-13 10:25:01 +0800

    Create navigation bar design

---
+++ src/lib/components/navigation.svelte
@@ -0,0 +1,32 @@
+<script>
+	import Button from './ui/button/button.svelte'
+
+	const links = [
+		{ label: '主页', to: '/' },
+		{ label: '社区论坛', to: '/community' },
+		{ label: '社团', to: '/clubs' },
+		{ label: '关于我们', to: '/about' }
+	]
+</script>
+
+<div class="flex min-h-14 w-full items-center justify-between bg-blue-50 px-20">
+	<div>
+		<a href="/" class="text-2xl font-bold">IC Portrayal</a>
+	</div>
+
+	<div class="flex gap-6">
+		{#each links as link}
+			<div
+				class="rounded-full px-4 py-2 transition-all duration-200 hover:bg-foreground hover:text-background"
+			>
+				<a href={link.to}>
+					{link.label}
+				</a>
+			</div>
+		{/each}
+	</div>
+
+	<div>
+		<Button href="/auth" variant="outline">登录</Button>
+	</div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script>
 	import '../app.css'
+	import Navigation from '$lib/components/navigation.svelte'
 </script>
 
-<slot></slot>
+<div class="flex flex-col min-h-svh h-1 w-full bg-slate-50">
+	<Navigation />
+	<div class="flex-1">
+		<slot />
+	</div>
+</div>
