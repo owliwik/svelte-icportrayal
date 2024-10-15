436e1a737ad0a21af1faf9f11666f0454f080d35
Owliwik Li <michael@owliwik.com>
2024-10-16 00:49:41 +0800

    Add responsive design to navigation bar

--- src/lib/components/navigation.svelte
+++ src/lib/components/navigation.svelte
@@ -1,5 +1,7 @@
 <script>
 	import Button from './ui/button/button.svelte'
+	import { Menu } from 'lucide-svelte'
+	import * as Sheet from './ui/sheet'
 
 	const links = [
 		{ label: '主页', to: '/' },
@@ -11,13 +13,13 @@
 
 <div class="absolute top-4 flex w-full justify-center">
 	<div
-		class="flex min-h-14 w-[55rem] items-center justify-between bg-primary-foreground bg-opacity-100 rounded-full px-20 backdrop-blur-lg"
+		class="mx-4 flex min-h-14 w-[55rem] items-center justify-between rounded-full bg-primary-foreground bg-opacity-100 px-12 backdrop-blur-lg"
 	>
 		<div>
 			<a href="/" class="text-2xl font-bold">IC Portrayal</a>
 		</div>
 
-		<div class="flex gap-6">
+		<div class="hidden gap-6 md:flex">
 			{#each links as link}
 				<div
 					class="rounded-full px-4 py-2 transition-all duration-200 hover:bg-foreground hover:text-background"
@@ -29,8 +31,37 @@
 			{/each}
 		</div>
 
-		<div>
+		<div class="hidden md:block">
 			<Button href="/auth" class="rounded-full px-5">登录</Button>
+		</div>
+
+		<div class="md:hidden">
+			<Sheet.Root>
+				<Sheet.Trigger>
+					<Button variant="ghost" size="icon">
+						<Menu />
+					</Button>
+				</Sheet.Trigger>
+
+				<Sheet.Content>
+					<div class="mx-8 my-12">
+
+						<div class="flex flex-col">
+							{#each links as link}
+								<div
+									class="rounded-full px-4 py-2 my-1 transition-all duration-200 hover:bg-foreground hover:text-background"
+								>
+									<a href={link.to}>
+										{link.label}
+									</a>
+								</div>
+							{/each}
+						</div>
+
+						<Button href="/auth" class="rounded-full mx-2 px-5 my-4">登录</Button>
+					</div>
+				</Sheet.Content>
+			</Sheet.Root>
 		</div>
 	</div>
 </div>
